3ca540f597511ff74b2b0f3eecd655bbb1f66358
ngocmon97 <104144571+ngocmon97@users.noreply.github.com>
2023-04-04 23:48:40 +0700

    Add files via upload
    update

---
+++ src/test/java/PbngocTest.java
@@ -0,0 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PbngocTest {
+    WebDriver driver;
+    @Test(priority = 1)
+    public void test1(){
+
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com");
+    }
+
+}
